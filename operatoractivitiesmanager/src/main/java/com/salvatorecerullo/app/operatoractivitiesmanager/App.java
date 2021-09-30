1e1562859a3cb2827fd2253c0eba454b61240e41
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-09-30 10:53:59 +0200

    Continus Integration setup: creation of test for App.java

--- operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/App.java
+++ operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/App.java
@@ -1,7 +1,11 @@
 package com.salvatorecerullo.app.operatoractivitiesmanager;
 
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public String sayHello() {
+		return "Hello";
+	}
 }
---
+++ operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/AppTest.java
@@ -0,0 +1,20 @@
+package com.salvatorecerullo.app.operatoractivitiesmanager;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+	private App app;
+
+	@Before
+	public void setup() {
+		app = new App();
+	}
+
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
