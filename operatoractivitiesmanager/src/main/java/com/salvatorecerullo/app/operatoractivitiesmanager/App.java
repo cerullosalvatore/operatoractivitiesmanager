6507a18681255e249f126cb31bafb9cb5a2d9e06
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-09-30 12:20:38 +0200

    Continus Integration setup: create .github folder with basic maven.yml

--- operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/App.java
+++ operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/App.java
@@ -1,10 +1,6 @@
 package com.salvatorecerullo.app.operatoractivitiesmanager;
 
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-
 	public String sayHello() {
 		return "Hello";
 	}
