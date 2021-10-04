1c5471505cd28b3122bc691470baaf481f35eeff
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-10-04 19:21:39 +0200

    Controller: creation directory and first controller's class.

---
+++ operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/controller/OperatorControllerTest.java
@@ -0,0 +1,13 @@
+package com.salvatorecerullo.app.operatoractivitiesmanager.controller;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class OperatorControllerTest {
+
+	@Test
+	public void test() {
+		fail("Failure, test must be implemented");
+	}
+}
