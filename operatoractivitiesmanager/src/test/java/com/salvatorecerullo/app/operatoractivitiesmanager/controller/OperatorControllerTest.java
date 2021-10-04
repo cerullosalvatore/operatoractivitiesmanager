8f1e22960c8d8f29e11721c9a74928b3a1c8f7ca
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-10-04 19:31:55 +0200

    Controller Operator: remove test failure

--- operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/controller/OperatorControllerTest.java
+++ operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/controller/OperatorControllerTest.java
@@ -1,13 +1,8 @@
 package com.salvatorecerullo.app.operatoractivitiesmanager.controller;
 
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class OperatorControllerTest {
 
-	@Test
-	public void test() {
-		fail("Failure, test must be implemented");
-	}
 }
