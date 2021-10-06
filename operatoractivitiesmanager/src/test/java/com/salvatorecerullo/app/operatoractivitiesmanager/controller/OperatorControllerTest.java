ae7976f3cdfeb0518ed3303692c4521351c96efa
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-10-06 12:50:25 +0200

    UT OperatorController: first of all we create new feature test.

--- operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/controller/OperatorControllerTest.java
+++ operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/controller/OperatorControllerTest.java
@@ -1,8 +1,14 @@
 package com.salvatorecerullo.app.operatoractivitiesmanager.controller;
 
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import com.salvatorecerullo.app.operatoractivitiesmanager.model.Operator;
 import com.salvatorecerullo.app.operatoractivitiesmanager.repository.OperatorRepository;
 import com.salvatorecerullo.app.operatoractivitiesmanager.view.OperatorView;
 
@@ -17,4 +23,9 @@ public class OperatorControllerTest {
 	@InjectMocks
 	private OperatorController operatorController;
 	
+	@Test
+	public void testAllOperators(){
+		List<Operator> operators = Arrays.asList(new Operator());
+		when(operatorRepository.findAll()).thenReturn(operators);
+	}
 }
