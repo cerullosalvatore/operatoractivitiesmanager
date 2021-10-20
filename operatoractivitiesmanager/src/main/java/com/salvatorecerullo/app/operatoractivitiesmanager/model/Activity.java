b5e563d5c69cc96f8ff7701ded919f0e338dfe34
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-10-20 17:18:25 +0200

    UnitTest ActivityMongoRepository: resolve code smells.

--- operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/model/Activity.java
+++ operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/model/Activity.java
@@ -74,15 +74,6 @@ public class Activity {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
 		Activity other = (Activity) obj;
 		if (endTime == null) {
 			if (other.endTime != null) {
