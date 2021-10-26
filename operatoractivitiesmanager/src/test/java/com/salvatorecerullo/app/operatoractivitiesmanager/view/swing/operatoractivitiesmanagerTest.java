ed39806dd97c9793e8a386f5613f7427d192e66c
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-10-26 12:02:11 +0200

    GUI Setup: First test. No implementation will therefore fail.

---
+++ operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/operatoractivitiesmanagerTest.java
@@ -0,0 +1,14 @@
+package com.salvatorecerullo.app.operatoractivitiesmanager.view.swing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class operatoractivitiesmanagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
