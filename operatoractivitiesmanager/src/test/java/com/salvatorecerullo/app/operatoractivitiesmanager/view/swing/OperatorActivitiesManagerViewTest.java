012170e9829e41a7364c710734d8687a5d5e9f1b
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-10-26 12:20:35 +0200

    GUI Setup: Override onSetUp method

--- operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/OperatorActivitiesManagerViewTest.java
+++ operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/OperatorActivitiesManagerViewTest.java
@@ -2,10 +2,37 @@ package com.salvatorecerullo.app.operatoractivitiesmanager.view.swing;
 
 import static org.junit.Assert.*;
 
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 
-public class OperatorActivitiesManagerViewTest {
+@RunWith(GUITestRunner.class)
+public class OperatorActivitiesManagerViewTest extends AssertJSwingJUnitTestCase{
 
+	@InjectMocks
+	private OperatorActivitiesManagerView operatorActivitiesManagerView;
+
+	private FrameFixture frameFixture;
+	
+	@Override
+	protected void onSetUp() throws Exception {
+		//Our frame and the fixture will be recreated for each test method so that we always start with a fresh user interface.
+		GuiActionRunner.execute(() -> {
+			operatorActivitiesManagerView = new OperatorActivitiesManagerView();
+			return operatorActivitiesManagerView;
+		});
+
+		MockitoAnnotations.initMocks(this);
+		//FrameFixture will then be used to interact with our view’s controls (labels, text fields, buttons, etc.).
+		frameFixture = new FrameFixture(robot(), operatorActivitiesManagerView);
+		frameFixture.show(); // shows the frame to test
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
