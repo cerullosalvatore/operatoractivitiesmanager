c9700a3211ba140feeffc77c9a87dbe849d5df86
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-11-11 17:23:54 +0100

    GUI UT: BasicOperationPanel add test and methods

--- operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/BasicOperationPanel.java
+++ operatoractivitiesmanager/src/main/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/BasicOperationPanel.java
@@ -3,6 +3,8 @@ package com.salvatorecerullo.app.operatoractivitiesmanager.view.swing;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.BorderLayout;
 import javax.swing.JList;
 import javax.swing.JTextField;
@@ -41,7 +43,7 @@ public class BasicOperationPanel extends JPanel {
 	private JButton btnModify;
 	private JButton btnDelete;
 	private JLabel lblMessageStatus;
-	
+	private Boolean updateInProgress;
 	private String basicOperationIdTemp;
 
 	/**
@@ -49,93 +51,152 @@ public class BasicOperationPanel extends JPanel {
 	 */
 	public BasicOperationPanel() {
 		basicOperationIdTemp = new ObjectId().toString();
+		updateInProgress = false;
 
 		setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		newBasicOperationPanel = new JPanel();
 		add(newBasicOperationPanel);
 		newBasicOperationPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		// LABEL OPERATION DETAILS
 		lblOperationDetails = new JLabel("BasicOperationDetails");
 		newBasicOperationPanel.add(lblOperationDetails, BorderLayout.NORTH);
-		
+
 		formBasicOperationPanel = new JPanel();
 		newBasicOperationPanel.add(formBasicOperationPanel);
 		formBasicOperationPanel.setLayout(new GridLayout(3, 2, 0, 0));
-		
+
 		// LABEL ID
 		lblId = new JLabel("ID:");
 		formBasicOperationPanel.add(lblId);
-		
+
 		// TEXT FIELD ID
 		textFieldId = new JTextField();
 		formBasicOperationPanel.add(textFieldId);
 		textFieldId.setColumns(10);
 		textFieldId.setText(basicOperationIdTemp);
 		textFieldId.setEditable(false);
-	
+
 		// LABEL NAME
 		lblName = new JLabel("Name:");
 		formBasicOperationPanel.add(lblName);
-		
+
 		// TEXT FIELD NAME
 		textFieldName = new JTextField();
 		formBasicOperationPanel.add(textFieldName);
 		textFieldName.setColumns(10);
-		
+		textFieldName.addKeyListener(getKeyListenerTextField());
+
 		// LABEL DESCRIPTION
 		lblDescription = new JLabel("Description:");
 		formBasicOperationPanel.add(lblDescription);
-		
+
 		// TEXT AREA DESCRIPTION
 		textAreaDescription = new JTextArea();
 		formBasicOperationPanel.add(textAreaDescription);
-		
+		textAreaDescription.addKeyListener(getKeyListenerTextField());
+
 		buttonsFormBasicOperationPanel = new JPanel();
 		newBasicOperationPanel.add(buttonsFormBasicOperationPanel, BorderLayout.SOUTH);
-		
+
 		btnAddOperation = new JButton("Add Operation");
 		buttonsFormBasicOperationPanel.add(btnAddOperation);
 		btnAddOperation.setEnabled(false);
-		
+
+		// BUTTON UPDATE OPERATIONS
 		btnUpdateOperation = new JButton("Update Operation");
 		buttonsFormBasicOperationPanel.add(btnUpdateOperation);
 		btnUpdateOperation.setEnabled(false);
+		btnUpdateOperation.addActionListener(e -> actionListenerUpdateButton());
 		
 		listBasicOperationsPanel = new JPanel();
 		add(listBasicOperationsPanel);
 		listBasicOperationsPanel.setLayout(new BorderLayout(0, 0));
-		
+
+		// LIST BASIC OPERATION
 		listBasicOpeationModel = new DefaultListModel<>();
 		listBasicOperations = new JList<>(listBasicOpeationModel);
 		listBasicOperationsPanel.add(listBasicOperations, BorderLayout.CENTER);
 		listBasicOperations.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listBasicOperations.addListSelectionListener(e -> actionListenerListOperators());
 
 		listBottomMenuPanel = new JPanel();
 		listBasicOperationsPanel.add(listBottomMenuPanel, BorderLayout.SOUTH);
 		listBottomMenuPanel.setLayout(new GridLayout(2, 0, 0, 0));
-		
+
 		listBottomMenuPanelButton = new JPanel();
 		listBottomMenuPanel.add(listBottomMenuPanelButton);
-		
+
+		// BUTTON MODIFY
 		btnModify = new JButton("MODIFY");
 		listBottomMenuPanelButton.add(btnModify);
 		btnModify.setEnabled(false);
-		
+		btnModify.addActionListener(e -> actionListenerModifyButton());
+
+		// BUTTON DELETE
 		btnDelete = new JButton("DELETE");
 		listBottomMenuPanelButton.add(btnDelete);
 		btnDelete.setEnabled(false);
-		
+
 		lblMessageStatus = new JLabel("");
 		listBottomMenuPanel.add(lblMessageStatus);
 
 		setNames();
 	}
-	
+
 	// GETTERS AND SETTERS
 	public String getBasicOperationIdTemp() {
 		return basicOperationIdTemp;
+	}
+
+	public DefaultListModel<BasicOperation> getListBasicOpeationModel() {
+		return listBasicOpeationModel;
+	}
+
+	// ACTION KEY LISTENERS
+
+	private KeyAdapter getKeyListenerTextField() {
+		return new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				btnAddOperation.setEnabled(setButtonAddEnabled());
+				btnUpdateOperation.setEnabled(setButtonUpdateEnabled());
+			}
+		};
+	}
+
+	// ACTION LISTENERSS
+
+	private void actionListenerModifyButton() {
+		updateInProgress = true;
+		
+		BasicOperation basicOperationSelected = listBasicOpeationModel.getElementAt(listBasicOperations.getSelectedIndex());
+
+		textFieldId.setText(basicOperationSelected.getId());
+		textFieldName.setText(basicOperationSelected.getName());
+		textAreaDescription.setText(basicOperationSelected.getDescription());
+
+		btnUpdateOperation.setEnabled(true);
+		btnAddOperation.setEnabled(false);
+		btnDelete.setEnabled(false);
+		listBasicOperations.setEnabled(false);
+	}
+
+	private void actionListenerUpdateButton() {
+		updateInProgress = false;
+		listBasicOperations.setEnabled(true);
+		basicOperationIdTemp = new ObjectId().toString();
+		textFieldId.setText(basicOperationIdTemp);
+		textFieldName.setText("");
+		textAreaDescription.setText("");
+		btnUpdateOperation.setEnabled(false);
+		btnAddOperation.setEnabled(false);
+	}
+
+	private void actionListenerListOperators() {
+		btnModify.setEnabled(listBasicOperations.getSelectedIndex() != -1);
+		btnDelete.setEnabled(listBasicOperations.getSelectedIndex() != -1);
 	}
 
 	// UTILITY
@@ -159,5 +220,17 @@ public class BasicOperationPanel extends JPanel {
 		btnModify.setName("btnModify");
 		btnDelete.setName("btnDelete");
 		lblMessageStatus.setName("lblMessageStatus");
+	}
+
+	private boolean setButtonAddEnabled() {
+		return !updateInProgress && statusFieldCompiled();
+	}
+
+	private boolean setButtonUpdateEnabled() {
+		return updateInProgress && statusFieldCompiled();
+	}
+
+	private boolean statusFieldCompiled() {
+		return !textFieldName.getText().isEmpty() && !textAreaDescription.getText().isEmpty();
 	}
 }
--- operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/BasicOperationPanelTest.java
+++ operatoractivitiesmanager/src/test/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/BasicOperationPanelTest.java
@@ -8,12 +8,13 @@ import org.assertj.swing.fixture.JTabbedPaneFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
 
+import com.salvatorecerullo.app.operatoractivitiesmanager.model.BasicOperation;
+
 public class BasicOperationPanelTest extends AssertJSwingJUnitTestCase {
 	private OperatorActivitiesManagerView operatorActivitiesManagerView;
 
 	private FrameFixture frameFixture;
 
-	
 	@Override
 	protected void onSetUp() {
 		// Our frame and the fixture will be recreated for each test method so that we
@@ -29,15 +30,17 @@ public class BasicOperationPanelTest extends AssertJSwingJUnitTestCase {
 		JTabbedPaneFixture tabbedPaneFixture = frameFixture.panel("contentPane").tabbedPane("tabbedPane");
 		tabbedPaneFixture.selectTab("Basic Operations");
 	}
-	
+
 	@Test
 	@GUITest
 	public void testBasicOperationControlsArePrestentAndInitialStates() {
 		// Verify
 		frameFixture.panel("newBasicOperationPanel").label("lblOperationDetails").requireVisible();
-		JPanelFixture formOBasicOperationPanel = frameFixture.panel("newBasicOperationPanel").panel("formBasicOperationPanel");
+		JPanelFixture formOBasicOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("formBasicOperationPanel");
 		formOBasicOperationPanel.label("lblId").requireVisible();
-		formOBasicOperationPanel.textBox("textFieldId").requireVisible().requireNotEditable().requireText(operatorActivitiesManagerView.getBasicOperationPanel().getBasicOperationIdTemp());
+		formOBasicOperationPanel.textBox("textFieldId").requireVisible().requireNotEditable()
+				.requireText(operatorActivitiesManagerView.getBasicOperationPanel().getBasicOperationIdTemp());
 		formOBasicOperationPanel.label("lblName").requireVisible();
 		formOBasicOperationPanel.textBox("textFieldName").requireVisible().requireEnabled().requireEmpty();
 		formOBasicOperationPanel.label("lblDescription").requireVisible();
@@ -49,10 +52,199 @@ public class BasicOperationPanelTest extends AssertJSwingJUnitTestCase {
 		JPanelFixture listBasicOperationsPanel = frameFixture.panel("listBasicOperationsPanel");
 		listBasicOperationsPanel.list("listBasicOperations").requireVisible().requireEnabled().requireNoSelection();
 		JPanelFixture listBottomMenuPanel = frameFixture.panel("listBottomMenuPanel");
-		listBottomMenuPanel.panel("listBottomMenuPanelButton").button("btnModify").requireVisible()
-				.requireDisabled();
-		listBottomMenuPanel.panel("listBottomMenuPanelButton").button("btnDelete").requireDisabled()
-				.requireDisabled();
+		listBottomMenuPanel.panel("listBottomMenuPanelButton").button("btnModify").requireVisible().requireDisabled();
+		listBottomMenuPanel.panel("listBottomMenuPanelButton").button("btnDelete").requireDisabled().requireDisabled();
 		listBottomMenuPanel.label("lblMessageStatus").requireVisible().requireText("");
 	}
+
+	@Test
+	@GUITest
+	public void testAddOperationButtonEnabledOnlyWhenAllInputIsCompiled() {
+		// Setup
+		JPanelFixture formOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("formBasicOperationPanel");
+		JPanelFixture buttonsFormBasicOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("buttonsFormBasicOperationPanel");
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").enterText("TestName");
+		formOperationPanel.textBox("textAreaDescription").enterText("TestDescription");
+
+		// Verify
+		buttonsFormBasicOperationPanel.button("btnAddOperation").requireEnabled();
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").deleteText();
+
+		// Verify
+		buttonsFormBasicOperationPanel.button("btnAddOperation").requireDisabled();
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").enterText("TestName");
+		formOperationPanel.textBox("textAreaDescription").deleteText();
+
+		// Verify
+		buttonsFormBasicOperationPanel.button("btnAddOperation").requireDisabled();
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").deleteText();
+
+		// Verify
+		buttonsFormBasicOperationPanel.button("btnAddOperation").requireDisabled();
+	}
+
+	@Test
+	@GUITest
+	public void testDeleteButtonAndModifyShouldBeEnabledOnlyWhenABasicOperationIsSelected() {
+		// Setup
+		JPanelFixture listBasicOperationsPanel = frameFixture.panel("listBasicOperationsPanel");
+		JPanelFixture listBottomMenuPanel = listBasicOperationsPanel.panel("listBottomMenuPanel");
+
+		BasicOperation newBasicOperation = new BasicOperation("IdOperation", "NameOperation", "TestDescription");
+		GuiActionRunner.execute(() -> {
+			operatorActivitiesManagerView.getBasicOperationPanel().getListBasicOpeationModel()
+					.addElement(newBasicOperation);
+		});
+
+		// Exercise
+		listBasicOperationsPanel.list("listBasicOperations").selectItem(0);
+
+		// Verify
+		listBottomMenuPanel.button("btnModify").requireEnabled();
+		listBottomMenuPanel.button("btnDelete").requireEnabled();
+
+		// Exercise
+		listBasicOperationsPanel.list("listBasicOperations").clearSelection();
+
+		// Verify
+		listBottomMenuPanel.button("btnModify").requireDisabled();
+		listBottomMenuPanel.button("btnDelete").requireDisabled();
+	}
+
+	@Test
+	@GUITest
+	public void testModifyButtonIsPressedListDisabledAndUpdateButtonIsEnabled() {
+		// Setup
+		JPanelFixture formOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("formBasicOperationPanel");
+		JPanelFixture buttonsFormBasicOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("buttonsFormBasicOperationPanel");
+
+		JPanelFixture listBasicOperationsPanel = frameFixture.panel("listBasicOperationsPanel");
+		JPanelFixture listBottomMenuPanel = listBasicOperationsPanel.panel("listBottomMenuPanel");
+
+		BasicOperation newBasicOperation = new BasicOperation("IdOperation", "NameOperation", "TestDescription");
+		GuiActionRunner.execute(() -> {
+			operatorActivitiesManagerView.getBasicOperationPanel().getListBasicOpeationModel()
+					.addElement(newBasicOperation);
+		});
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").enterText("TestName");
+		formOperationPanel.textBox("textAreaDescription").enterText("TestDescription");
+
+		listBasicOperationsPanel.list("listBasicOperations").selectItem(0);
+		listBottomMenuPanel.button("btnModify").click();
+
+		// Verify
+		listBasicOperationsPanel.list("listBasicOperations").requireDisabled();
+		buttonsFormBasicOperationPanel.button("btnUpdateOperation").requireEnabled();
+		buttonsFormBasicOperationPanel.button("btnAddOperation").requireDisabled();
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").deleteText();
+
+		// Verify
+		buttonsFormBasicOperationPanel.button("btnUpdateOperation").requireDisabled();
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").enterText("TestName");
+		formOperationPanel.textBox("textAreaDescription").deleteText();
+
+		// Verify
+		buttonsFormBasicOperationPanel.button("btnUpdateOperation").requireDisabled();
+
+	}
+
+	@Test
+	@GUITest
+	public void testUpdateOperationIsPressedListEnabledAndInputReset() {
+		// Setup
+		JPanelFixture formOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("formBasicOperationPanel");
+		JPanelFixture buttonsFormBasicOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("buttonsFormBasicOperationPanel");
+
+		JPanelFixture listBasicOperationsPanel = frameFixture.panel("listBasicOperationsPanel");
+		JPanelFixture listBottomMenuPanel = listBasicOperationsPanel.panel("listBottomMenuPanel");
+
+		BasicOperation newBasicOperation = new BasicOperation("IdOperation", "NameOperation", "TestDescription");
+		GuiActionRunner.execute(() -> {
+			operatorActivitiesManagerView.getBasicOperationPanel().getListBasicOpeationModel()
+					.addElement(newBasicOperation);
+		});
+
+		// Exercise
+		formOperationPanel.textBox("textFieldName").enterText("TestName");
+		formOperationPanel.textBox("textAreaDescription").enterText("TestDescription");
+
+		listBasicOperationsPanel.list("listBasicOperations").selectItem(0);
+		listBottomMenuPanel.button("btnModify").click();
+
+		buttonsFormBasicOperationPanel.button("btnUpdateOperation").click();
+
+		// Verify
+		listBasicOperationsPanel.list("listBasicOperations").isEnabled();
+		formOperationPanel.textBox("textFieldId")
+				.requireText(operatorActivitiesManagerView.getBasicOperationPanel().getBasicOperationIdTemp());
+		formOperationPanel.textBox("textFieldName").requireEmpty();
+		formOperationPanel.textBox("textAreaDescription").requireEmpty();
+
+		buttonsFormBasicOperationPanel.button("btnUpdateOperation").requireDisabled();
+		buttonsFormBasicOperationPanel.button("btnAddOperation").requireDisabled();
+	}
+
+	@Test
+	@GUITest
+	public void testModifyButtonIsPressedFieldWasCompiledCorrectly() {
+		// Setup
+		JPanelFixture formOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("formBasicOperationPanel");
+		JPanelFixture buttonsFormBasicOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("buttonsFormBasicOperationPanel");
+
+		JPanelFixture listBasicOperationsPanel = frameFixture.panel("listBasicOperationsPanel");
+		JPanelFixture listBottomMenuPanel = listBasicOperationsPanel.panel("listBottomMenuPanel");
+
+		BasicOperation basicOperation1 = new BasicOperation("IdOperation1", "NameOperation1", "TestDescription1");
+		BasicOperation basicOperation2 = new BasicOperation("IdOperation2", "NameOperation2", "TestDescription2");
+
+		GuiActionRunner.execute(() -> {
+			operatorActivitiesManagerView.getBasicOperationPanel().getListBasicOpeationModel()
+					.addElement(basicOperation1);
+			operatorActivitiesManagerView.getBasicOperationPanel().getListBasicOpeationModel()
+					.addElement(basicOperation2);
+		});
+
+		// Exercise
+		listBasicOperationsPanel.list("listBasicOperations").selectItem(0);
+		listBottomMenuPanel.button("btnModify").click();
+
+		// Verify
+		formOperationPanel.textBox("textFieldId").requireText(basicOperation1.getId());
+		formOperationPanel.textBox("textFieldName").requireText(basicOperation1.getName());
+		formOperationPanel.textBox("textAreaDescription").requireText(basicOperation1.getDescription());
+
+		// Exercise
+		buttonsFormBasicOperationPanel.button("btnUpdateOperation").click();
+		listBasicOperationsPanel.list("listBasicOperations").selectItem(1);
+		listBottomMenuPanel.button("btnModify").click();
+
+		// Verify
+		formOperationPanel.textBox("textFieldId").requireText(basicOperation2.getId());
+		formOperationPanel.textBox("textFieldName").requireText(basicOperation2.getName());
+		formOperationPanel.textBox("textAreaDescription").requireText(basicOperation2.getDescription());
+	}
+
+	
 }
