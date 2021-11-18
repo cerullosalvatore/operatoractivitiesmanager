0ce5db94c048cda8bdc419c84c98896c5d51e17b
Salvatore <salvatore.cerullo@stud.unifi.it>
2021-11-18 10:23:04 +0100

    GUI IT: BasicOperationPanel Add & Delete Integration Test

--- operatoractivitiesmanager/src/it/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/BasicOperationPanelIT.java
+++ operatoractivitiesmanager/src/it/java/com/salvatorecerullo/app/operatoractivitiesmanager/view/swing/BasicOperationPanelIT.java
@@ -9,6 +9,7 @@ import org.assertj.swing.core.Robot;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.fixture.JListFixture;
+import org.assertj.swing.fixture.JPanelFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
@@ -26,21 +27,21 @@ import com.salvatorecerullo.app.operatoractivitiesmanager.repository.mongo.Basic
 import de.bwaldvogel.mongo.bson.ObjectId;
 
 @RunWith(GUITestRunner.class)
-public class BasicOperationPanelIT  extends AssertJSwingJUnitTestCase {
+public class BasicOperationPanelIT extends AssertJSwingJUnitTestCase {
 	private static final String BASICOPERATION = "The BasicOperation with ID: ";
 	private static final String NOTEXIST = " does not exist.";
-	
+
 	private JFrame jFrame;
 	private BasicOperationPanel basicOperationPanel;
 	private FrameFixture frameFixture;
 
 	private BasicOperationRepository basicOperationRepository;
 	private BasicOperationController basicOperationController;
-	
+
 	private static final String DB_NAME = "operatoractivities";
 	private static final String COLLECTION_NAME = "basicoperation";
 	private static int mongoPort = Integer.parseInt(System.getProperty("mongo.port", "27017"));
-	
+
 	@Override
 	protected void onSetUp() {
 		MockitoAnnotations.initMocks(this);
@@ -48,7 +49,7 @@ public class BasicOperationPanelIT  extends AssertJSwingJUnitTestCase {
 		basicOperationRepository = new BasicOperationMongoRepository(mongoClient, DB_NAME, COLLECTION_NAME);
 		MongoDatabase database = mongoClient.getDatabase(DB_NAME);
 		database.drop();
-		
+
 		// Our frame and the fixture will be recreated for each test method so that we
 		// always start with a fresh user interface.
 		GuiActionRunner.execute(() -> {
@@ -75,7 +76,7 @@ public class BasicOperationPanelIT  extends AssertJSwingJUnitTestCase {
 		BasicOperation basicOperation2 = new BasicOperation(new ObjectId().toString(), "name2", "description2");
 		basicOperationRepository.save(basicOperation1);
 		basicOperationRepository.save(basicOperation2);
-		
+
 		// Exercise
 		GuiActionRunner.execute(() -> {
 			basicOperationController.allBasicOperations();
@@ -83,10 +84,117 @@ public class BasicOperationPanelIT  extends AssertJSwingJUnitTestCase {
 
 		// Verify
 		JListFixture listBasicOperations = frameFixture.panel("listBasicOperationsPanel").list("listBasicOperations");
-		assertThat(listBasicOperations.contents()).containsExactly(basicOperation1.toString(), basicOperation2.toString());
+		assertThat(listBasicOperations.contents()).containsExactly(basicOperation1.toString(),
+				basicOperation2.toString());
+	}
+
+	@Test
+	@GUITest
+	public void testAddButtonSuccess() {
+		// Setup
+		BasicOperation basicOperation1 = new BasicOperation(new ObjectId().toString(), "name1", "description1");
+		String idTemp = basicOperationPanel.getBasicOperationIdTemp();
+		BasicOperation basicOperationNew = new BasicOperation(idTemp, "nameNew", "descriptionNew");
+		basicOperationRepository.save(basicOperation1);
+
+		// Exercise
+		JPanelFixture formOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("formBasicOperationPanel");
+		JPanelFixture buttonsFormBasicOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("buttonsFormBasicOperationPanel");
+
+		formOperationPanel.textBox("textFieldName").enterText("nameNew");
+		formOperationPanel.textBox("textAreaDescription").enterText("descriptionNew");
+
+		buttonsFormBasicOperationPanel.button("btnAddOperation").click();
+
+		// Verify
+		JListFixture listBasicOperations = frameFixture.panel("listBasicOperationsPanel").list("listBasicOperations");
+		assertThat(listBasicOperations.contents()).containsExactly(basicOperation1.toString(),
+				basicOperationNew.toString());
+
+		JPanelFixture listBottomMenuPanel = frameFixture.panel("listBasicOperationsPanel").panel("listBottomMenuPanel");
+		listBottomMenuPanel.label("lblMessageStatus")
+				.requireText(BASICOPERATION + basicOperationNew.getId() + " has been added.");
+	}
+
+	@Test
+	@GUITest
+	public void testAddButtonError() {
+		// Setup
+		String idTemp = basicOperationPanel.getBasicOperationIdTemp();
+		BasicOperation basicOperation1 = new BasicOperation(idTemp, "name1", "description1");
+		BasicOperation basicOperationNew = new BasicOperation(idTemp, "nameNew", "descriptionNew");
+		basicOperationRepository.save(basicOperation1);
+
+		// Exercise
+		JPanelFixture formOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("formBasicOperationPanel");
+		JPanelFixture buttonsFormBasicOperationPanel = frameFixture.panel("newBasicOperationPanel")
+				.panel("buttonsFormBasicOperationPanel");
+
+		formOperationPanel.textBox("textFieldName").enterText("nameNew");
+		formOperationPanel.textBox("textAreaDescription").enterText("descriptionNew");
+
+		buttonsFormBasicOperationPanel.button("btnAddOperation").click();
+
+		// Verify
+		JListFixture listBasicOperations = frameFixture.panel("listBasicOperationsPanel").list("listBasicOperations");
+		assertThat(listBasicOperations.contents()).containsExactly(basicOperation1.toString());
+
+		JPanelFixture listBottomMenuPanel = frameFixture.panel("listBasicOperationsPanel").panel("listBottomMenuPanel");
+		listBottomMenuPanel.label("lblMessageStatus")
+				.requireText(BASICOPERATION + basicOperationNew.getId() + " already exist.");
+	}
+
+	@Test
+	@GUITest
+	public void testDeleteButtonSuccess() {
+		// Setup
+		BasicOperation basicOperation1 = new BasicOperation(new ObjectId().toString(), "name1", "description1");
+
+		// Exercise
+		GuiActionRunner.execute(() -> {
+			basicOperationController.addBasicOperation(basicOperation1);
+		});
+
+		JPanelFixture listBasicOperationsPanel = frameFixture.panel("listBasicOperationsPanel");
+		JPanelFixture listBottomMenuPanel = listBasicOperationsPanel.panel("listBottomMenuPanel");
+		// Exercise
+		listBasicOperationsPanel.list("listBasicOperations").selectItem(0);
+		listBottomMenuPanel.button("btnDelete").click();
+
+		// Verify
+		JListFixture listBasicOperations = frameFixture.panel("listBasicOperationsPanel").list("listBasicOperations");
+		assertThat(listBasicOperations.contents()).isEmpty();
+
+		listBottomMenuPanel.label("lblMessageStatus")
+				.requireText(BASICOPERATION + basicOperation1.getId() + " has been removed.");
 	}
 	
-	
-	
-	
+	@Test
+	@GUITest
+	public void testDeleteButtonError() {
+		// Setup
+		BasicOperation basicOperation1 = new BasicOperation(new ObjectId().toString(), "name1", "description1");
+
+		// Exercise
+		GuiActionRunner.execute(() -> {
+			basicOperationPanel.getListBasicOpeationModel().addElement(basicOperation1);
+		});
+
+		JPanelFixture listBasicOperationsPanel = frameFixture.panel("listBasicOperationsPanel");
+		JPanelFixture listBottomMenuPanel = listBasicOperationsPanel.panel("listBottomMenuPanel");
+		// Exercise
+		listBasicOperationsPanel.list("listBasicOperations").selectItem(0);
+		listBottomMenuPanel.button("btnDelete").click();
+
+		// Verify
+		JListFixture listBasicOperations = frameFixture.panel("listBasicOperationsPanel").list("listBasicOperations");
+		assertThat(listBasicOperations.contents()).isEmpty();
+
+		listBottomMenuPanel.label("lblMessageStatus")
+				.requireText(BASICOPERATION + basicOperation1.getId() + NOTEXIST);
+	}
+
 }
